8a078803394df3265bd34c8eea00b92fd637bf72
SpyMaster356 <stephen-356@hotmail.com>
2012-10-08 17:43:15 -0400

    V0.0.0.1
    Commit of empty files
    Includes:
    Empty unity project
    C# project
    -Parcer

---
+++ Project files/Assets/CodeSystem/Parcer.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+using System;
+
+public class Parcer : MonoBehaviour {
+	
+	public static void parceCode(String rawCode) {
+        String[] keywords;
+        
+        // put each line into a String array
+		String[] lines = rawCode.Split('\n');
+
+        //go though each line
+        for (int i = 0; i < lines.Length; i++) {
+            parceLine(lines[i]);
+        }
+	}
+
+    private static void parceLine(String line)
+    {
+        //clean out white space
+        while (line.StartsWith("\t") || line.StartsWith(" ")) {
+            line = line.Substring(1);
+        }
+
+
+    }
+}
